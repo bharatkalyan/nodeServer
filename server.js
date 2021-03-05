9bf7f87c8298ab41c68f31df6c30495b20a1c214
bharatkalyan <s.bharatkalyan@gmail.com>
2021-03-05 14:40:31 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+var cors = require('cors')
+app.use(cors())
+const port = process.env.PORT || 3001;
+app.use(express.static(__dirname + '/dist/statisticsOfVirus'));
+app.get('/', (req, res) =>res.sendFile(path.join(__dirname)));
+const server = http.createServer(app);
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
